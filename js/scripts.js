cb3dc4885e3baf9940fa27590ca8b528e02a03ac
Stephan Khoury <stephane.khoury@gmail.com>
2016-10-19 13:15:41 -0700

    add multiplications and divisions oh so successfully tgod

--- js/scripts.js
+++ js/scripts.js
@@ -34,3 +34,21 @@ $(document).ready(function() {
     $("#output2").text(result);
   });
 });
+$(document).ready(function() {
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#multiply1").val());
+    var number2 = parseInt($("#multiply2").val());
+    var result = multiply(number1, number2);
+    $("#output3").text(result);
+  });
+});
+$(document).ready(function() {
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#divide1").val());
+    var number2 = parseInt($("#divide2").val());
+    var result = divide(number1, number2);
+    $("#output4").text(result);
+  });
+});
